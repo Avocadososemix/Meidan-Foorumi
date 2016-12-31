be01517df280e7fb7c2b15cf223d45da804c0666
Irene Nikkarinen <irene.nikkarinen@helsinki.fi>
2016-12-31 15:13:12 +0200

    Update Main.java

--- Meidan-Foorumi lopullinen palautus/Meidan-Foorumi-master/src/main/java/tikape/runko/Main.java
+++ Meidan-Foorumi lopullinen palautus/Meidan-Foorumi-master/src/main/java/tikape/runko/Main.java
@@ -58,38 +58,10 @@ public class Main {
         }, new ThymeleafTemplateEngine());
 
         post("/alue/:alueid", (req, res) -> { //alueen id  --> näyttää keskustelut
-            // vanha metodi, joka tallensi suoraan --> Integer keskustelunavausId = keskustelunavausDao.tallenna(req.queryParams("keskustelunavaus"), Integer.parseInt(req.params(":alueid")));
-            //req.params palauttaa Stringin, jonka Integer.parseInt muuttaa luvuksi
-            //String keskustelu = req.queryParams("keskustelunavaus"); //tallentaa syötetyn keskustelunavauksen nimen
-            //keskustelu = keskustelu.trim();
-            /*if (keskustelu.length() < 3 || keskustelu.length() > 35) {                  // ei tyhjää otsikkoa
-                res.redirect("/alue/" + req.params(":alueid"));
-                return "ok";
-            }*/
             res.redirect("/alue/" + req.params(":alueid") + "/luouusikeskustelu"); //ohjataan luomaan viesti --keskustelunavauksen id
             return "ok";
         });
-        
-        /*get("/alue/:alueid/keskustelu/:keskusteluid", (req, res) -> { 
-            HashMap map = new HashMap<>();
-            map.put("viestit", viestiDao.etsiKeskustelunViestit(Integer.parseInt(req.params(":alueid")), Integer.parseInt(req.params(":keskusteluid")))); //ei toimi
-            map.put("keskustelu", keskustelunavausDao.etsi(Integer.parseInt(req.params(":keskusteluid"))));
-            map.put("alue", alueDao.etsi(Integer.parseInt(req.params(":alueid"))));
-            
-            return new ModelAndView(map, "viestit");
-        }, new ThymeleafTemplateEngine());
-        
-        post("/alue/:alueid/keskustelu/:keskusteluid", (req, res) -> {
-            String lähettäjä = req.queryParams("lähettäjä");
-            String viesti = req.queryParams("viesti");
-            if (lähettäjä.trim().length() != 0 && viesti.trim().length() != 0) { //ei tyhjää viestiä tai lähettäjää
-                viestiDao.tallenna(req.queryParams("lähettäjä"), req.queryParams("viesti"), Integer.parseInt(req.params(":keskusteluid"))); //Viesti (aika, lähettäjä, viesti, keskustelunavaus)
-            }
-            res.redirect("/alue/" + req.params(":alueid") + "/keskustelu/" + Integer.parseInt(req.params(":keskusteluid")));
-            return "ok";
-        });
-        */
-        
+                
         get("/alue/:alueid/luouusikeskustelu", (req, res) -> { //näyttää sivun, jossa pyydetään keskustelunavaukselle 
             HashMap map = new HashMap<>();                                      //viestin ennen kuin se luodaan
             //String keskusteluNimi = req.params(":keskusteluNimi");
